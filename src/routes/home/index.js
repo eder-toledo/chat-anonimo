f058862f5a6f5d43ad1c62a11cedddce2f3f46cd
eder-toledo <me890506@gmail.com>
2020-07-23 23:16:08 -0500

    Configuración de elementos basicos para la página Home

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
-const Page = ({ match }) => (
+const Page = () => (
     <div>
-        <p>Home</p>
+        <Box component="span" p={1} mx="auto">
+            <Typography variant="h3">
+                Bienvenido a Chat anónimo
+            </Typography>
+
+            <Typography variant="subtitle">
+                Para poder comenzar a chatear necesitas ingresar tu Nickname y presionar en comenzar.
+            </Typography>
+            <br></br>
+            <TextField id="standard-basic" label="Nickname" />
+            <br></br>
+            <Button color="primary">
+                Comenzar
+            </Button>
+        </Box>
     </div>
 )
 
